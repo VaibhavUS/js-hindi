a922160eb2d4e5a1f89c3cb3019ae6e7456f38b0
Vaibhav Gurav <49413894+VaibhavUS@users.noreply.github.com>
2025-08-01 21:50:32 +0000

    nums and maths added

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,21 @@
+// NUMBERS AND MATHS IN JS
+
+
+/* ******************** NUMBERS ******************** */
+
+const score = 400 // this is automatically defined as number
+console.log(score)
+
+const balance = new Number(100) // but in this case we have specially mentioned this as a number
+console.log(balance);
+
+console.log(balance.toString().length) // converted number into string using toString() to use string operations
+
+console.log(balance.toFixed(2)) // to control decimal digits or precision value
+
+const otherNumber = 123.8966
+console.log(otherNumber.toPrecision(3)) // It will take the value given in precision and round-off the last digit
+
+
+
+
