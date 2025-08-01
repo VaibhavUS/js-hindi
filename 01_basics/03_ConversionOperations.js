8e4c3e8a13c57e038fc0dad28fe517222f0209d8
Vaibhav Gurav <49413894+VaibhavUS@users.noreply.github.com>
2025-08-01 05:17:32 +0000

    Operations added

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -1,5 +1,8 @@
 // CONVERSION AND OPERATIONS IN JS
 
+
+/* ******************** CONVERSION ******************** */
+
 // -> Conversion in JS means converting string into number or vice-versa or conversion like this
 
 let score = "33"
@@ -57,3 +60,51 @@ SUMMARY FOR TYPE CONVERSION
 3) "vaibhav" => true
 
 */
+
+
+
+
+/* ******************** OPERATIONS ******************** */
+
+// -> Adding, Substracting number; connecting two string means operations
+
+let value = 3
+let negValue = -value
+console.log(negValue) // O/p -> -3
+
+console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2*2)
+console.log(2**3) // to get power
+console.log(2%3) // to get remainder
+
+// Connecting two strings
+
+let str1 = "Hello"
+let str2 = " Vaibhav"
+
+let str3 = str1 + str2
+console.log(str3)
+
+// Some number and string behaviour in JS
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+
+// Prefix and Postfix
+
+/* Postfix */
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+/* Prefix */
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+
+
